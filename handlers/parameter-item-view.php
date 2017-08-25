9f236a6069d1d8554eb486746e9e8d674a0967aa
jeypips <johnpaulbalanon6@gmail.com>
2017-08-25 11:03:34 +0800

    .

--- handlers/parameter-item-view.php
+++ handlers/parameter-item-view.php
@@ -9,7 +9,12 @@ session_start();
 $con = new pdo_db();
 
 $parameter_items = $con->getData("SELECT * FROM parameter_items WHERE item_id = $_POST[item_id]");
+$parameter = $con->getData("SELECT parameter_id, parameter_name FROM parameters WHERE parameter_id = ".$parameter_items[0]['item_parameter']);
+
+$parameter_items[0]['item_parameter'] = $parameter[0];
+
 
 echo json_encode($parameter_items[0]);
 
 ?>
+
