a109a2f42c251ba1feef5497286257e650092e90
Theroshi <theroshaballesteros@gmail.com>
2017-10-27 16:07:50 +0800

    .

--- profile.php
+++ profile.php
@@ -157,7 +157,7 @@
 							<div class="col-lg-3">
 									<ul class="list-group contacts-list">
 									<li class="list-group-item" style="cursor: pointer;" ng-class="{'active': subMenuList.profile}" ng-click="form.activateForm(this,'profile',profile)">
-										<a href="javascript:;"><h4 class="text text-inverse text-center active"><b><i class="glyphicon glyphicon-home pull-left"></i>{{profile.municipality.municipality}}</b></h4></a>
+										<a href="javascript:;"><h4 class="text text-inverse text-center active"><b>Basic Information</b></h4></a>
 									</li>
 									<li class="list-group-item" style="cursor: pointer;" ng-class="{'active': subMenuList[s.sector_shortname]}" ng-repeat="s in sectors" ng-click="form.activateForm(this,s.sector_shortname,profile)">
 										<a href="javascript:;">
