8fb088cbe3ffb5049e326e375fea6e39bdb6397a
jeypips <johnpaulbalanon6@gmail.com>
2017-11-17 08:14:13 +0800

    ..

--- index.php
+++ index.php
@@ -216,6 +216,10 @@
         <script src="assets/plugins/flot-chart/jquery.flot.orderBars.min.js"></script>
         <script src="assets/plugins/flot-chart/jquery.flot.crosshair.js"></script>	
 		
+		<!-- Chart JS -->
+        <script src="assets/plugins/Chart.js/Chart.min.js"></script>
+        <script src="assets/pages/chartjs.init.js"></script>
+		
 		<!-- Angular  -->
 		<script src="angular/angular.min.js"></script>
 		<script src="modules/fullscreen.js"></script>
@@ -225,10 +229,8 @@
 		<script src="modules/account.js"></script>
 		<script src="modules/notifications.js"></script>
 		<script src="modules/charts.js"></script>
-		<script src="modules/admincharts.js"></script>
-
+		<script src="modules/linegraph.js"></script>
 		<script src="modules/landcharts.js"></script>
-
 		<script src="modules/dashboard.js"></script>
 		<script src="controllers/dashboard.js"></script>
     </body>
